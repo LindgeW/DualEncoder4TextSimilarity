aff14dacd21f99da07220aef3791f16f51591b61
Lam chi <tjuwlz2020@163.com>
2021-03-14 19:35:11 +0800

    Update QAMatcher.py

--- model/QAMatcher.py
+++ model/QAMatcher.py
@@ -155,18 +155,3 @@ def label_smoothing_loss(pred, target, classes, smoothing=0.1, dim=-1):
     return torch.mean(torch.sum(-true_dist * pred, dim=dim))
 
 
-class LabelSmoothingLoss(nn.Module):
-    def __init__(self, classes, smoothing=0.0, dim=-1):
-        super(LabelSmoothingLoss, self).__init__()
-        self.smoothing = smoothing
-        self.cls = classes
-        self.dim = dim
-
-    def forward(self, pred, target):
-        pred = pred.log_softmax(dim=self.dim)
-        with torch.no_grad():
-            # true_dist = pred.data.clone()
-            true_dist = torch.zeros_like(pred)
-            true_dist.fill_(self.smoothing / (self.cls - 1))
-            true_dist.scatter_(1, target.data.unsqueeze(1), 1.0 - self.smoothing)
-        return torch.mean(torch.sum(-true_dist * pred, dim=self.dim))
